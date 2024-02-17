563e0ed8dbe5a1af345c603eeb2cc0dcf35a5dfe
ThugXD <vvalterzandamela@gmail.com>
2024-02-17 23:05:58 +0200

    50Project->25Days

---
+++ 10_Joke_gos_here/scripts.js
@@ -0,0 +1,19 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn  = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+//Using ASYNC/AWAIT
+async function generateJoke(){
+    const config ={
+        headers: {
+            Accept: 'aplication/json',
+        }, 
+    }
+    const res = await fetch('https://icanhazdadjoke.com', config)
+    const data = await res.json()
+
+    jokeEl.innerHTML = data.joke
+}
